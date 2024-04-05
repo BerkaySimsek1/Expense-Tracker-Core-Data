96f0664236c0419a58ed8f747edeeb39ada61318
BerkaySimsek1 <berkay.simsek.098@gmail.com>
2024-04-05 14:09:11 +0300

    Initial Commit

---
+++ Expense Tracker Core Data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Tracker Core Data
+//
+//  Created by Berkay on 5.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Tracker Core Data/Expense_Tracker_Core_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_Core_DataApp.swift
+//  Expense Tracker Core Data
+//
+//  Created by Berkay on 5.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_Core_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
